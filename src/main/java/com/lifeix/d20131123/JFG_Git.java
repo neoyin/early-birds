48a4dc7371fa4f56b4d5eb3966b4ed4171e38bbc
Lifeix <jiangfuqiang24@gmail.com>
2013-11-23 14:31:37 +0800

    提交代码

--- src/main/java/com/lifeix/d20131123/JFG_Git.java
+++ src/main/java/com/lifeix/d20131123/JFG_Git.java
@@ -2,6 +2,9 @@ package com.lifeix.d20131123;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
 
 public class JFG_Git {
 
@@ -14,13 +17,27 @@ public class JFG_Git {
 		while(is.read(buffer) > -1) {
 			sb.append(new String(buffer));
 		}
-		System.out.println(sb.toString());
+		String[] lines = sb.toString().split("\n");
+		int count = 0;
+		Map<Integer, String> reduceMap = new HashMap<Integer, String>(lines.length);
+		Map<Integer, String> addMap = new HashMap<Integer, String>(lines.length);
+		for(String line : lines) {
+			if(line.startsWith("-")) {
+				reduceMap.put(++count, line);
+			} else if(line.startsWith("+")) {
+				addMap.put(++count, line);
+			}
+		}
+		printMsg(reduceMap, "减少");
+		printMsg(addMap, "增加");
 	}
 	
-	/**
-	 * @param args
-	 * @throws IOException 
-	 */
+	private void printMsg(Map<Integer, String> data,String flag) {
+		for(Entry<Integer, String> entry : data.entrySet()) {
+			System.out.println("在第" + entry.getKey() + "行 " + flag + " " + entry.getValue());
+		}
+	}
+	
 	public static void main(String[] args) throws IOException {
 		new JFG_Git().print();
 	}
