bcc60141b8d665d95fb71fded5fcfb644dd8de28
Anwj <loveanwanjun@163.com>
2013-11-30 15:57:51 +0800

    awj commit

--- src/main/java/com/lifeix/d20131130/AWJ_Train.java
+++ src/main/java/com/lifeix/d20131130/AWJ_Train.java
@@ -2,36 +2,16 @@ package com.lifeix.d20131130;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Scanner;
 
 public class AWJ_Train implements TrainTest{
 
 	@Override
 	public boolean check(String mn, String trains) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	public static void main(String[] args) {
-		/*Scanner scanner = new Scanner(System.in);
-		String str1,str2 = "";
-		int m,n = 0;
-		System.out.println("请输入列车数量和调度站可容纳列车数量（n m）,空格分隔，如：4 5");
-		str1 = scanner.next();
-		System.out.println(str1);
-		System.out.println("请输入列车驶出顺序，空格分隔，如：1 3 2 4 5");
-		str2 = scanner.next();*/
-		System.out.println(manage("4 3", "1 4 2 3"));
-		
-	}
-
-	public static boolean manage(String str1, String str2){
-		String [] array = str1.split(" ");
+		String [] array = mn.split(" ");
 		int n = Integer.valueOf(array[0]);//列车数量
 		int m = Integer.valueOf(array[1]);//中转站车位
-		String [] order = str2.split(" ");
+		String [] order = trains.split(" ");
 		List<String> list =  Arrays.asList(order);
 
 		//调度站车位数组
@@ -65,5 +45,11 @@ public class AWJ_Train implements TrainTest{
 
 		return true;
 	}
+
+	public static void main(String[] args) {
+		System.out.println(new AWJ_Train().check("4 3", "1 4 2 3"));
+		
+	}
+
 
 }
