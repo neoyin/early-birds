61ac304fe3f1e09fe45f2c1035bd87c5334c5223
Anwj <loveanwanjun@163.com>
2013-11-30 17:04:00 +0800

    awj

--- src/main/java/com/lifeix/d20131130/AWJ_Train.java
+++ src/main/java/com/lifeix/d20131130/AWJ_Train.java
@@ -21,23 +21,30 @@ public class AWJ_Train implements TrainTest{
 			return false;
 		}else{
 			for(int i=0; i<n; i++){
+				System.out.println(order[i]);
 				if(Integer.valueOf(order[i]) != i+1){
 					if(schedule.contains(order[i])){
 						schedule.remove(order[i]);
+						//System.out.println(i);
+						System.out.println(order[i]);
 						out.add(order[i]);
 						continue;
 					}
 					for(int j=Integer.valueOf(order[i])-1; j>0;j--){
 						//如果调度前面的列车没在调度站或没被开出，则将列车停在调度站
-						if(!schedule.contains(j) || !out.contains(String.valueOf(i+1))){
-							System.out.println(schedule.size());
+						if(!schedule.contains(j) && !out.contains(String.valueOf(j))){
+							System.out.println(schedule);
 							if(schedule.size() > m){
 								return false;
 							}else{
-								schedule.add(String.valueOf(j));
+								if(!schedule.contains(String.valueOf(j))){
+									schedule.add(String.valueOf(j));
+								}
 							}
 						}
 					}
+					out.add(order[i]);
+
 				}else{
 					out.add(order[i]);
 				}
@@ -49,7 +56,7 @@ public class AWJ_Train implements TrainTest{
 
 	public static void main(String[] args) {
 		System.out.println(new AWJ_Train().check("5 2", "3 1 4 5 2"));
-		
+
 	}
 
 
