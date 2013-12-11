fc90fb937ee52e5a3d3d3a5b7681827f79568d56
jiangfuqiang <jiangfuqiang24@gmail.com>
2013-12-11 18:55:04 +0800

    jfq

---
+++ src/main/java/com/lifeix/d20131211/JFQ_F.java
@@ -0,0 +1,37 @@
+package com.lifeix.d20131211;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class JFQ_F {
+
+	public static int cycle(int n) {
+		if(n <= 2){  
+            return 1;  
+        }else{  
+            return cycle(n-1) + cycle(n-2);  
+        }  
+	}
+	
+	public static int noCycle(int n) {
+		if(n <= 2){  
+            return 1;  
+        }  
+        int n1 = 1;
+        int n2 = 1;
+        int sn = 0;  
+        for(int i = 0; i < n - 2; i ++){  
+            sn = n1 + n2;  
+            n1 = n2;  
+            n2 = sn;  
+        }  
+        return sn;  
+	}
+	
+	public static void main(String[] args) {
+		int n = 10;
+        System.out.println(cycle(n)); 
+		System.out.println(noCycle(n));
+	}
+
+}
