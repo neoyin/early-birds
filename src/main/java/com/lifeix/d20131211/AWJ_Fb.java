20a610dc1d6ed1b3f3066f82a0b1cab4d642458d
Anwj <loveanwanjun@163.com>
2013-12-11 19:07:31 +0800

    awj commit

---
+++ src/main/java/com/lifeix/d20131211/AWJ_Fb.java
@@ -0,0 +1,39 @@
+package com.lifeix.d20131211;
+
+public class AWJ_Fb {
+
+
+	public static void main(String[] args) {
+		AWJ_Fb test = new AWJ_Fb();
+		System.out.println(test.rb(40));
+		System.out.println(test.unfb(40));
+	}
+
+	public int rb(int n){
+		if(n<=2){
+			return 1;
+		}else if(n>2){
+			return rb(n-1)+rb(n-2);
+		}else if(n<0){
+			System.out.println("输入有误");
+			return -1;
+		}
+		return 0;
+	}
+
+	public Long unfb(int n) {
+		if (n <= 2)
+			return 1L;
+
+		long l1 = 1;
+		long l2 = 1;
+		int index = 2;
+		while (index < n - 1) {
+			long l = l2;
+			l2 = l1 + l2;
+			l1 = l;
+			index++;
+		}
+		return l1 + l2;
+	}
+}
