12af7f5efffbe34d452e5e7bc42c61ef26a9e122
taketoheart <suhongj09@gmail.com>
2013-12-18 19:39:04 +0800

    delete d20131218

--- src/main/java/com/lifeix/d20131218/awj/MyTime.java
+++
@@ -1,9 +0,0 @@
-package com.lifeix.d20131218.awj;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
-@Retention(RetentionPolicy.RUNTIME)
-public @interface MyTime {
-
-}
