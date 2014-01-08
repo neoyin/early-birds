4d3b2fb7a346a36dace63810eb6c16136b8b1266
jiangfuqiang <jiangfuqiang24@gmail.com>
2014-01-08 18:50:17 +0800

    jfq

---
+++ src/main/java/com/lifeix/d20140108templet/AbstractTemplate.java
@@ -0,0 +1,25 @@
+package com.lifeix.d20140108templet;
+
+public abstract class AbstractTemplate {
+	
+	private void boil() {
+		System.out.println("开始烧开水...");
+	}
+	
+	private void pushCup() {
+		System.out.println("倒入被子.....");
+	}
+	
+	private void mixture(String mertial) {
+		System.out.println("混合【" + mertial + "】和开水.....");
+	}
+	
+	protected void coffeOrTea(String mertial) {
+		boil();
+		mixture(mertial);
+		pushCup();
+		System.out.println("请您慢用......总共100元...");
+		System.out.println("--__--.............");
+	}
+	
+}
