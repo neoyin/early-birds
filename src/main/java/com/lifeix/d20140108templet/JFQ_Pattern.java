b21b568e2e799077550685b7e3025c760ca2d6a6
jiangfuqiang <jiangfuqiang24@gmail.com>
2014-01-08 18:49:46 +0800

    jfq

---
+++ src/main/java/com/lifeix/d20140108templet/JFQ_Pattern.java
@@ -0,0 +1,15 @@
+package com.lifeix.d20140108templet;
+
+public class JFQ_Pattern extends AbstractTemplate{
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JFQ_Pattern jp = new JFQ_Pattern();
+		jp.coffeOrTea("咖啡");
+		jp.coffeOrTea("茶");
+	}
+
+}
