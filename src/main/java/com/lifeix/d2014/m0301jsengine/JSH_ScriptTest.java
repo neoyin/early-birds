823fd8573f098881a078a09c6c395c97a3f33829
taketoheart <suhongj09@gmail.com>
2014-03-01 13:57:44 +0800

    jsh

---
+++ src/main/java/com/lifeix/d2014/m0301jsengine/JSH_ScriptTest.java
@@ -0,0 +1,29 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+import javax.script.Invocable;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
+public class JSH_ScriptTest {
+
+	/**
+	 * @param args
+	 * @throws ScriptException 
+	 * @throws FileNotFoundException 
+	 * @throws NoSuchMethodException 
+	 */
+	public static void main(String[] args) throws ScriptException, FileNotFoundException, NoSuchMethodException {
+		FileReader fileReader = new FileReader(new File("md5.js"));
+		ScriptEngineManager manager = new ScriptEngineManager();
+		ScriptEngine engine = manager.getEngineByName("JavaScript");
+		engine.put("window", "getWindowBase()");
+		engine.eval(fileReader);
+		Invocable ivo = (Invocable)engine;
+		String result = String.valueOf(ivo.invokeFunction("md5", "123"));
+		System.out.println(result);
+	}
+
+}
