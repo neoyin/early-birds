119a8ae6b0c1f0f545616d4225536f4e48cce4e4
Anna-mm <ajiao5198@163.com>
2014-03-29 17:21:59 +0800

    ajiao

---
+++ src/main/js/d2014/0329chromeextention/catchbox/content.js
@@ -0,0 +1,10 @@
+﻿setInterval(function() {
+	var xbox = document.getElementById("X_BOX_IMG");
+
+	if(xbox && xbox.style.display != "none") {
+		var event = document.createEvent("MouseEvents");
+		event.initMouseEvent("click",true,false,document.defaultView,0,0,0,0,0,false,false,false,false,0,null);
+		xbox.dispatchEvent(event);
+	}
+},1000);
+
